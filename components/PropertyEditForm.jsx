8faba6c1f85ff49afd3040e546451edfddc93e50
Ognjen <contact.ognjen@gmail.com>
2024-03-22 11:02:31 +0100

    Update PUT API Route

--- components/PropertyEditForm.jsx
+++ components/PropertyEditForm.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
-import { fetchProperties, fetchProperty } from '@/utils/request';
+import { fetchProperty } from '@/utils/request';
 
 const PropertyEditForm = () => {
   const { id } = useParams();
@@ -114,7 +114,29 @@ const PropertyEditForm = () => {
     }));
   };
 
-  const handleSubmit = async () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData(e.target);
+
+      const res = await fetch(`/api/properties/${id}`, {
+        method: 'PUT',
+        body: formData,
+      });
+
+      if (res.status === 200) {
+        router.push(`/properties/${id}`);
+      } else if (res.status === 401 || res.status === 403) {
+        toast.error('Permission denied');
+      } else {
+        toast.error('Something went wrong');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    }
+  };
 
   return (
     mounted &&
