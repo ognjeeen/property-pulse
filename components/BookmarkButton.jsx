5430aaad761b5336d5c4e16012cb3b767f60d070
Ognjen <contact.ognjen@gmail.com>
2024-03-25 11:25:01 +0100

    Check bookmark status and toggle button

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -11,6 +11,41 @@ const BookmarkButton = ({ property }) => {
   const userId = session?.user?.id;
 
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // If 401 error is there when not logged in, this is the fix
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+
+    const checkBookmarkStatus = async () => {
+      try {
+        const res = await fetch('/api/bookmarks/check', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            propertyId: property._id,
+          }),
+        });
+
+        // If successfull get data so you can set isBookmarked
+        if (res.status === 200) {
+          const data = await res.json();
+          setIsBookmarked(data.isBookmarked);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkBookmarkStatus();
+  }),
+    [property._id, userId];
 
   // Handle function for bookmark
   const handleClick = async () => {
@@ -43,7 +78,16 @@ const BookmarkButton = ({ property }) => {
     }
   };
 
-  return (
+  if (loading) return <p className="text-center">Loading...</p>;
+
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Remove Bookmark
+    </button>
+  ) : (
     <button
       onClick={handleClick}
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
