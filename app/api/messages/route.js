5ea33c8f1603dfb8030d44b66f3f72375f3e2684
Ognjen <contact.ognjen@gmail.com>
2024-03-27 12:17:20 +0100

    Fetch messages

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -4,6 +4,38 @@ import { getSessionUser } from '@/utils/getSessionUser';
 
 export const dynamic = 'force-dynamic';
 
+// GET /api/messages
+export const GET = async () => {
+  try {
+    await connectDB();
+
+    // Getting a session user
+    const sessionUser = await getSessionUser();
+
+    // Checking to see if there is a session and session user
+    if (!sessionUser || !sessionUser.user) {
+      return new Response('User ID is required', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Finding message thath matches recipient id
+    // Populate that object with sender name and property title
+    const messages = await Message.find({
+      recipient: userId,
+    })
+      .populate('sender', 'name')
+      .populate('property', 'title');
+
+    return new Response(JSON.stringify(messages), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', {
+      status: 500,
+    });
+  }
+};
+
 // POST /api/messages
 export const POST = async (request) => {
   try {
